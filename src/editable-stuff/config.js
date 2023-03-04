a1080f2679e50b32b8d63463c769fc0613af62b0
Naqeeb Ahmed <naqeeb532@outlook.com>
2023-03-05 03:25:50 +0530

    few changes

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -9,7 +9,7 @@ const mainBody = {
   firstName: "Naqeeb",
   middleName: "",
   lastName: "Ahmed",
-  message: " A keen and curious learner ",
+  message: " Data Science And Machine Learning Enthusiast.",
   icons: [
     {
       image: "fa-github",
@@ -54,7 +54,7 @@ const about = {
   imageLink: require("../editable-stuff/naqeeb.jpg"),
   imageSize: 375,
   message:
-    "Hey, I'm Naqeeb Ahmed. Currently in the pre final year of my undergraduate degree in Electronics & Communication Engineering from Guru Gobind Singh Indraprastha University, New Delhi. I would describe myself as someone who is deeply fascinated by Technology and Innovation. I'm most passionate about Data Science, Machine Learning, Robotics and Competitive Programming. In my free time, I explore other new tech innovations like cloud computing, Quantum computing and IoT etc",
+    "Hi, I'm Naqeeb Ahmed, a Junior year Electronics & Communication Engineering student at Guru Gobind Singh Indraprastha University in New Delhi. I am deeply interested in Technology and Innovation, particularly in Data Science, Machine Learning, and Research projects. My goal is to work as a Data Scientist and I have prior experience as a Data Science Intern at Algo Analytica. In my free time, I continue to explore new techs such as cloud computing, Quantum computing, and IoT.",
   resume: "https://drive.google.com/file/d/1679RmHE-gpI5JD6r8tEJU3Ji6xEQHQbp/view?usp=share_link",
 };
 
@@ -68,7 +68,7 @@ const repos = {
   heading: "Recent Projects",
   gitHubUsername: "naqeeb2710", //i.e."johnDoe12Gh"
   reposLength: 0,
-  specificRepos: ["X-Ray-Pneumonia-Prediction-CNN","Pong-Game-ML","NaqeebBlog","ADGITM-NOTICE-Discord-bot","Twitter-Sentiment-Analysis","Hack-sih","Delhi-housing-price"],
+  specificRepos: ["X-Ray-Pneumonia-Prediction-CNN","Pong-Game-ML","NaqeebBlog","ADGITM-NOTICE-Discord-bot","Twitter-Sentiment-Analysis","Delhi-housing-price","Hack-sih"],
 };
 
 // Leadership SECTION
@@ -102,24 +102,26 @@ const skills = {
   hardSkills: [
     { name: "Python", value: 90 },
     { name: "Data Structures", value: 85 },
+    { name: "Object Oriented Programming", value: 80 },
     { name: "C/C++", value: 85 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "Arduino", value: 80 },
+    { name: "Web Developmet", value: 70 },
     { name: "Data Visulaization", value: 90 },
     { name: "TensorFlow", value: 80 },
     { name: "Deep Leaaring", value: 80 },
-    { name: "CNN", value: 80 },
     { name: "Natural Language Processing", value: 80 },
+    { name: "Open CV", value: 80 },
+    { name: "Computer Vision", value: 75 },
+    { name: "Transfer Learning", value: 80 },
     { name: "Sentiment Analysis", value: 90 },
     { name: "Clustering and Classification Analysis", value: 90 },
     { name: "Mathematics and Statistics.", value: 80 },
-    { name: "Machine Learning", value: 80 },
-    { name: "SQL", value: 80 },
+    { name: "SQL", value: 70 },
+    { name: "Power Bi", value: 80 },
     { name: "Excel", value: 80 },
     { name: "Linux", value: 80 },
+    { name: "Flask", value: 70 },
     { name: "AWS", value: 40 },
+    { name: "Docker", value: 80 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
@@ -140,7 +142,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for Internship in Software Engineering, Data Science or in Machine learning.! If you know of any positions available, or if you have any questions, or if you just want to say hi, please feel free to email me at",
+    "I'm currently looking for Internship as a Data Scientist, Machine Learning Engineer or as Software Engineer. If you know of any positions available, or if you have any questions, or if you just want to say hi, please feel free to email me at",
   email: "naqeeb2710@gmail.com",
 };
 
