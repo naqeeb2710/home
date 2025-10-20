2ce5014dfb9d88461401c7f210a88f7498724398
shauryaM65 <hitesh.kumar@eulermotors.com>
2025-10-21 04:47:59 +0530

    Updates

---
+++ precache-manifest.03c69177f62440f0db456dafa897b237.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334174aa7497fb1b55c611e4887f6a51",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "930b7440b8193f0dc1cc",
+    "url": "/home/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "e4ba3468e5f3595572a5",
+    "url": "/home/static/css/main.39c39d30.chunk.css"
+  },
+  {
+    "revision": "930b7440b8193f0dc1cc",
+    "url": "/home/static/js/2.bd5a8448.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.bd5a8448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ba3468e5f3595572a5",
+    "url": "/home/static/js/main.aea82ff2.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "080e2e1569a5ea411d2fa08a63c4514a",
+    "url": "/home/static/media/congress.080e2e15.png"
+  },
+  {
+    "revision": "7d8b7f06dce76a80716a1a8342115d9d",
+    "url": "/home/static/media/naqeeb.7d8b7f06.jpg"
+  },
+  {
+    "revision": "ebd71f6bb602632dc071673474911a7b",
+    "url": "/home/static/media/resume.ebd71f6b.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e193e7f6236e080ab39d3cac6bd120c.js"
+  "/home/precache-manifest.03c69177f62440f0db456dafa897b237.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
